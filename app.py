7e681f10582a51132baa28c70e9686080c73c6c4
Anna Bowers <eenabow@gmail.com>
2021-04-07 19:33:32 -0400

    First push to branch group project work into my own repo

---
+++ app.py
@@ -0,0 +1,82 @@
+from sqlalchemy import create_engine
+from flask import Flask, jsonify, render_template
+
+#################################################
+# Database Setup
+#################################################
+connection_string = "postgres:270504@localhost:5432/climate_change_db"
+engine = create_engine(f'postgresql://{connection_string}')
+
+connection = engine.connect()
+#################################################
+# Flask Setup
+#################################################
+app = Flask(__name__)
+#################################################
+# Flask Routes
+#################################################
+
+
+@app.route('/')
+def index():
+    return render_template('welcome.html')
+
+# Top 20 CO2 connection
+@app.route("/top20CO2")
+def TopC02():
+
+    result = connection.execute(
+        """SELECT country_code, country_name, "2015", color FROM carbon_dioxide_top20 ORDER BY "2015" DESC LIMIT 20;""")
+    data = []
+    for row in result:
+        my_dict = {
+            "country_code": row[0],
+            "country_name": row[1],
+            "2015": float(row[2]),
+            "color": row[3]
+
+        }
+        data.append(my_dict)
+        print(row)
+    return(jsonify(data))
+
+# Top 20 GDP connection
+@app.route("/top20GDP")
+def TopGDP():
+
+    result = connection.execute(
+        """SELECT country_code, country_name, color, "2015" FROM gdp ORDER BY "2015" DESC LIMIT 20;""")
+    data = []
+    for row in result:
+        my_dict = {
+            "country_code": row[0],
+            "country_name": row[1],
+            "country_gdp": float(row[3]),
+            "color": row[2]
+        }
+        data.append(my_dict)
+        print(row)
+    return(jsonify(data))
+
+# Top 20 Renewable Energy connection
+@app.route("/top20Energy")
+def TopRenewableEnergy():
+
+    result = connection.execute(
+        """SELECT country_code, country_name, color, "2015" FROM renewable_energy ORDER BY "2015" DESC LIMIT 20;""")
+    data = []
+    for row in result:
+        my_dict = {
+            "country_code": row[0],
+            "country_name": row[1],
+            "country_energy": float(row[3]),
+            "color": row[2]
+        }
+        data.append(my_dict)
+        print(row)
+    return(jsonify(data))
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
---
+++ static/js/climate.js
@@ -0,0 +1,268 @@
+// // PIE CHART FOR CO2 data 
+
+function d3PieChart(){
+  url = "/top20CO2"
+  d3.json(url, function(data) {
+      
+      console.log(data);
+
+      // set the dimensions and margins of the graph
+      var width = 2800;
+      var height = 800;
+      var margin = 40;
+
+      // The radius of the pieplot is half the width or half the height (smallest one). I subtract a bit of margin.
+      var radius = Math.min(width, height) / 2 - margin;
+      console.log("here");
+      // append the svg object to the div called 'pieChart'
+      var svg = d3.select(".pieChart")
+      .append("svg")
+        .attr("width", width)
+        .attr("height", height)
+      .append("g")
+        .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+
+      // Create dummy data
+      var pie_data = {}
+      data
+      // .slice(0,4)
+      .map(obj=>{
+          pie_data[obj.country_code] = obj["2015"]
+      });
+
+      // set the color scale
+      var color = d3.scaleOrdinal()
+      .domain(pie_data)
+      .range(data.map(obj=>obj.color))
+      
+
+
+      // Compute the position of each group on the pie:
+      var pie = d3.pie()
+      .value(function(d) {return d.value; })
+      var data_ready = pie(d3.entries(pie_data))
+
+      // Build the pie chart: Basically, each part of the pie is a path that we build using the arc function.
+      
+      var arcGenerator = d3.arc()
+        .innerRadius(0)
+        .outerRadius(radius)
+      
+      svg.selectAll('mySlices')
+      .data(data_ready)
+      .enter()
+      .append('path')
+      .attr('d', arcGenerator
+      .innerRadius(0)
+      .outerRadius(radius)
+      )
+      .attr('fill', function(d){ return(color(d.data.key)) })
+      .attr("stroke", "black")
+      .style("stroke-width", "2px")
+      .style("opacity", 0.7)
+
+      svg.selectAll('mySlices')
+      .data(data_ready)
+      .enter()
+      .append('text')
+      .text(function(d){ return (d.data.key)})
+      // .attr("x",300)
+      // .attr("y",300)
+      // .attr("fill", "white")
+      .attr("transform", function(d) { return "translate(" + arcGenerator.centroid(d) + ")";  })
+      .style("text-anchor", "middle")
+      .style("font-size", 11)
+
+
+      
+  })
+}
+
+
+d3PieChart();
+ 
+
+
+
+
+
+
+// BAR CHART 
+function d3GDPbar() {
+url = "/top20GDP"
+d3.json(url, function (data) {
+  console.log(data);
+  // set the dimensions and margins of the graph
+  var margin = { top: 30, right: 30, bottom: 55, left: 90 },
+    width = 1000 - margin.left - margin.right,
+    height = 700 - margin.top - margin.bottom;
+  // append the svg object to the body of the page
+  var svg = d3.select(".GDPBar")
+    .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform",
+      "translate(" + margin.left + "," + margin.top + ")");
+  // // Parse the Data
+  
+  data
+    
+  // Add X axis
+  var x = d3.scaleLinear()
+    .domain([-6, 8])
+    .range([0, width]);
+  svg.append("g")
+    .attr("transform", "translate(0," + height + ")")
+    .call(d3.axisBottom(x))
+    .selectAll("text")
+    .attr("transform", "translate(-10,0)rotate(-45)")
+    .style("text-anchor", "end")
+    .style("font-size", "20px")
+    .attr("fill", "white") 
+    
+
+    svg.append("text")             
+      .attr("transform",
+            "translate(" + (width/2) + " ," + 
+                           (height + margin.top + 20) + ")")
+      .style("text-anchor", "middle")
+      .style("font-size", "22px")
+      .attr("fill", "white")  
+      .text("GDP");
+  
+
+  // Y axis
+  var y = d3.scaleBand()
+    .range([0, height])
+    .domain(data.map(function (d) { return d.country_code; }))
+    .padding(.1);
+  svg.append("g")
+    .call(d3.axisLeft(y))
+    .selectAll("text")
+    .style("font-size", "20px")
+    .attr("fill", "white") 
+    svg.append("text")
+    .attr("transform", "rotate(-90)")
+    .attr("y", 0 - margin.left)
+    .attr("x",0 - (height / 2))
+    .attr("dy", "1em")
+    .style("text-anchor", "middle")
+    .style("font-size", "22px")
+    .attr("fill", "white")  
+    .text("Country Code");
+    svg.append("text")
+    .attr("x", (width / 2))             
+    .attr("y", 0 - (margin.top / 2))
+    .attr("text-anchor", "middle")  
+    .style("font-size", "20px") 
+    .style("text-decoration", "underline")
+    .attr("fill", "white")  
+    .text("GDP PER COUNTRY");     
+  
+
+
+  //Bars
+  svg.selectAll(".GDPBar")
+    .data(data)
+    .enter()
+    .append("rect")
+    .attr("x", function (d) { return y(d.country_gdp)})
+    .attr("y", function (d) { return y(d.country_code); })
+    .attr("width", function (d) { return x(d.country_gdp); })
+    .attr("height", y.bandwidth())
+    .attr("fill", "#69b3a2")
+})
+};
+d3GDPbar();
+
+
+
+
+function d3EnergyBar() {
+  url = "/top20Energy"
+  d3.json(url, function (data) {
+    console.log(data);
+    // set the dimensions and margins of the graph
+    var margin = { top: 30, right: 30, bottom: 55, left: 90 },
+      width = 1000 - margin.left - margin.right,
+      height = 700 - margin.top - margin.bottom;
+    // append the svg object to the body of the page
+    var svg = d3.select(".RenewableBar")
+      .append("svg")
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)
+      .append("g")
+      .attr("transform",
+        "translate(" + margin.left + "," + margin.top + ")");
+    // // Parse the Data
+    
+  
+    
+    data
+      
+    // Add X axis
+    var x = d3.scaleLinear()
+      .domain([-2, 50])
+      .range([0, width]);
+    svg.append("g")
+      .attr("transform", "translate(0," + height + ")")
+      .call(d3.axisBottom(x))
+      .selectAll("text")
+      .attr("transform", "translate(-10,0)rotate(-45)")
+      .style("text-anchor", "end")
+      .style("font-size", "20px")
+      .attr("fill", "white") 
+  
+      svg.append("text")             
+        .attr("transform",
+              "translate(" + (width/2) + " ," + 
+                             (height + margin.top + 20) + ")")
+        .style("text-anchor", "middle")
+        .style("font-size", "22px")
+        .attr("fill", "white")  
+        .text("Renewable Energy Consumption");
+    // Y axis
+    var y = d3.scaleBand()
+      .range([0, height])
+      .domain(data.map(function (d) { return d.country_code; }))
+      .padding(.1);
+    svg.append("g")
+      .call(d3.axisLeft(y))
+      .style("font-size", "20px")
+      .selectAll("text")
+      .style("fill", "white") 
+      svg.append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 0 - margin.left)
+      .attr("x",0 - (height / 2))
+      .attr("dy", "1em")
+      .style("text-anchor", "middle")
+      .style("font-size", "22px")
+      .attr("fill", "white")  
+      .text("Country Code"); 
+    
+      svg.append("text")
+      .attr("x", (width / 2))             
+      .attr("y", 0 - (margin.top / 2))
+      .attr("text-anchor", "middle")  
+      .style("font-size", "20px") 
+      .attr("fill", "white")
+      .style("text-decoration", "underline")  
+      .text("RENEWABLE ENERGY CONSUMPTION PER COUNTRY");     
+    
+  
+  
+    //Bars
+    svg.selectAll(".RenewableBar")
+      .data(data)
+      .enter()
+      .append("rect")
+      .attr("x", function (d) { return y(d.country_energy)})
+      .attr("y", function (d) { return y(d.country_code); })
+      .attr("width", function (d) { return x(d.country_energy); })
+      .attr("height", y.bandwidth())
+      .attr("fill", "#69b3a2")
+  })
+  };
+  d3EnergyBar();
